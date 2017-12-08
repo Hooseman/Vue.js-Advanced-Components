4d1a102f037479639fb733a25642c4cd33c865f4
Juston Perkins <Hooseman@users.noreply.github.com>
2017-12-08 08:18:02 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+ <div class="container">
+   <div class="row">
+     <div class="col-xs-12">
+       <app-quote>
+         <H1 slot="title"> {{ quoteTitle }}</H1>
+         <p slot="content">Good to see you today</p>
+       </app-quote>
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue';
+
+  export default {
+    data:function() {
+      return {
+        quoteTitle:'Hello World!'
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+
+</style>
